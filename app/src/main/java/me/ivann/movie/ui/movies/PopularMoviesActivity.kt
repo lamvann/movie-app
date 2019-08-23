b7b216404d113023836fe1c45021ba536cc865fd
Ivann Ruiz <eruiz063@fiu.edu>
2019-08-22 21:41:21 -0400

    Revert to XML, remove all Anko Layouts code and imports

--- app/src/main/java/me/ivann/movie/ui/movies/PopularMoviesActivity.kt
+++ app/src/main/java/me/ivann/movie/ui/movies/PopularMoviesActivity.kt
@@ -1,24 +1,34 @@
 package me.ivann.movie.ui.movies
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.lifecycle.Observer
+import me.ivann.movie.R
 import me.ivann.movie.ui.base.BaseActivity
-import org.jetbrains.anko.setContentView
+import me.ivann.movie.util.extension.bindView
 
 
 class PopularMoviesActivity : BaseActivity<PopularMoviesViewModel>() {
 
-    private val ui by lazy { PopularMoviesUI() }
+    private val tvMainText: TextView by bindView(R.id.mainText)
+    private val btnGetMovies: Button by bindView(R.id.mainButton)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ui.setContentView(this)
+        setContentView(R.layout.activity_main)
+        setupListener()
         viewModel.movieData.observe(this, Observer {
             updateText(it)
         })
     }
 
+    private fun setupListener() =
+        btnGetMovies.setOnClickListener {
+            viewModel.getPopularMovies(autoDisposable)
+        }
+
     private fun updateText(content: String) {
-        ui.tvMainText.text = content
+        tvMainText.text = content
     }
 }
